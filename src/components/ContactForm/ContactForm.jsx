7e378e149d298bb6a35b509b299a08858ef3b096
ElenaLatynina <latynina.elena.v@gmail.com>
2022-12-08 20:33:10 +0200

    fixing

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -34,7 +34,7 @@ export const ContactForm = () => {
 
     const checkContact = newContact => {
         if (contacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
-            alert(`${newContact} is already in your contacts`);
+            alert(`You have this contact in your phonebook`);
             return;
         }
 
@@ -63,6 +63,8 @@ export const ContactForm = () => {
                     <Input
                         type="tel"
                         name="number"
+                        value={number}
+                        onChange ={handleChange}
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
